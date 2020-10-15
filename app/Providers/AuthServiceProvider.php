e141b3d7053d4be96d8e5210e12857e20d4ae284
Saidur Rahman <mmsaidurr@gmail.com>
2020-10-16 00:11:42 +0600

    first commit

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Question;
+use App\Policies\QuestionPolicy;
+use App\Answer;
+use App\Policies\AnswerPolicy;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        // 'App\Model' => 'App\Policies\ModelPolicy',
+        Question::class => QuestionPolicy::class,
+        Answer::class => AnswerPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        /*\Gate::define('update-question', function($user, $question) {
+            return $user->id === $question->user_id;
+        });
+
+        \Gate::define('delete-question', function($user, $question) {
+            return $user->id === $question->user_id;
+        });*/
+    }
+}
